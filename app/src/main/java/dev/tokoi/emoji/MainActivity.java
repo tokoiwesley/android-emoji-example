cff6bfaff237ea5640c98d61aeb6475cf50dde29
Wesley Tokoi <tokoiwesley@gmail.com>
2021-08-09 08:43:10 +0300

    Initialize emoji popup

--- app/src/main/java/dev/tokoi/emoji/MainActivity.java
+++ app/src/main/java/dev/tokoi/emoji/MainActivity.java
@@ -3,12 +3,53 @@ package dev.tokoi.emoji;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.vanniktech.emoji.EmojiPopup;
+import com.vanniktech.emoji.EmojiTextView;
 
 public class MainActivity extends AppCompatActivity {
+    // Initialize variables
+    ImageView mEmojiBt, mSendBt;
+    EditText mEmojiEt;
+    LinearLayout mLinearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Assign variables
+        mEmojiBt = findViewById(R.id.bt_emoji);
+        mSendBt = findViewById(R.id.bt_send);
+        mEmojiEt = findViewById(R.id.et_emoji);
+        mLinearLayout = findViewById(R.id.linear_layout);
+
+        // Initialize emoji popup
+        EmojiPopup popup = EmojiPopup.Builder
+                .fromRootView(findViewById(R.id.root_view))
+                .build(mEmojiEt);
+
+        mEmojiBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Toggle between text and emoji
+                popup.toggle();
+            }
+        });
+
+        mSendBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Inflate emoji text view
+                EmojiTextView emojiTextView = (EmojiTextView) LayoutInflater
+                        .from(v.getContext())
+                        .inflate(R.layout.emoji_text_view, mLinearLayout, false);
+            }
+        });
     }
 }
